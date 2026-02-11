a7442c388835ad117639f0b1c10e03ed22967edf
Sankalp-G <sankalp.gawali@gmail.com>
2026-02-12 00:49:50 +0530

    feat: add sample route again

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("app", "routes/app.tsx")] satisfies RouteConfig;
+export default [
+	index("routes/home.tsx"),
+	route("app", "routes/app.tsx"),
+	route("app/sample", "routes/app.sample.tsx"),
+] satisfies RouteConfig;
